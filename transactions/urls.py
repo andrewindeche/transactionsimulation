0728b5ed70068ffbe259607a62a36f0e1d5e4caf
andrewindeche <indecheandrew@gmail.com>
2025-02-20 23:02:25 +0300

    Add urls for transaction history

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,11 +1,12 @@
 # transactions/urls.py
 
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, AccountView, TransactionView
+from .views import UserRegisterView, UserLoginView, AccountView, TransactionView, TransactionHistoryView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('account/', AccountView.as_view(), name='account'),
     path('transaction/', TransactionView.as_view(), name='transaction'),
+    path('transactions/', TransactionHistoryView.as_view(), name='transaction_history'),
 ]
