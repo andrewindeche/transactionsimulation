c67d0a45b2a964fa80477ae980cc543a3208b937
andrewindeche <indecheandrew@gmail.com>
2025-02-20 22:48:22 +0300

    create views

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from .models import User, Transaction, Account
+from .serializers import UserSerializer, TransactionSerializer, AccountSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class AccountView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_object(self):
+        return self.request.user.account
+
+class TransactionView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
