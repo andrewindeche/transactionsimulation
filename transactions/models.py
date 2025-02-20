1a99b64eb7befffbf20d6ebbeece2aafe172f1d4
andrewindeche <indecheandrew@gmail.com>
2025-02-20 22:15:50 +0300

    Add user model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
