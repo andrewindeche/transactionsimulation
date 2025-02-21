a0b3d429ace70965591d43a8d7f763df946adc07
andrewindeche <indecheandrew@gmail.com>
2025-02-21 19:57:54 +0300

    resolve lint bugs

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,7 +1,15 @@
-# transactions/urls.py
-
+"""
+This module contains URL patterns for user registration, login,
+account management, transaction management, and transaction history views.
+"""
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, AccountView, TransactionView, TransactionHistoryView
+from .views import (
+    UserRegisterView,
+    UserLoginView,
+    AccountView,
+    TransactionView,
+    TransactionHistoryView
+)
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
