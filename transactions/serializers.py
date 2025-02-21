01b792f2c35d9339a52d6806bdfbf948024e7269
andrewindeche <indecheandrew@gmail.com>
2025-02-21 06:52:02 +0300

    Add validations for user inputs

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import User, Account, Transaction
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'username': {'required': True},
+            'email': {'required': True},
+            'password': {'write_only': True, 'required': True}
+        }
+        
+    def validate_password(self, value):
+        """
+        Django inbuilt password validation
+        """
+        validate_password(value)
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
