ba2eb66efd01d37e22f0fdadc42ed4c2e7d1f853
andrewindeche <indecheandrew@gmail.com>
2025-11-07 10:34:04 +0300

    configure celery

---
+++ transaction_simulation/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+
+# set the Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transaction_simulation.settings')
+
+# create Celery app and configure it from Django settings
+app = Celery('transaction_simulation')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(['transactions'])
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'transactions.User'
